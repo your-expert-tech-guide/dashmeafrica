09e6a007caba7b6d3515bb74a6fcb32b38443a6c
mutiat <mutiat900@gmail.com>
2023-12-18 01:18:14 +0100

    created all the login component

--- your-project-name/src/App.jsx
+++ your-project-name/src/App.jsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import {Route, Routes} from 'react-router-dom';
+
+
+
+// importing pages
+
+import Login from './Login'
+import Signup1 from './Signup1'
+import Signup2 from './Signup2';
+import Forgotpassword from './Forgotpassword'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <Routes>
+        <Route path='/signup1' element={<Signup1 />} />
+        <Route path='/signup2' element={<Signup2 />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/forgotpassword' element={<Forgotpassword />} />
+       
+</Routes>
     </>
-  )
+    
+  );
 }
 
-export default App
+export default App;
---
+++ your-project-name/src/PasswordInput.jsx
@@ -0,0 +1,47 @@
+// PasswordInput.jsx
+import React, { useState } from 'react';
+import hide from './assets/hide.png';
+
+const PasswordInput = ({ placeholder }) => {
+  const [password, setPassword] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleTogglePassword = () => {
+    setShowPassword(!showPassword);
+  };
+
+  return (
+    <div className="relative">
+      <input
+        type={showPassword ? 'text' : 'password'}
+        placeholder={placeholder}
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        className="border border-gray-300 rounded-md p-2 pl-10 focus:outline-none focus:ring focus:border-blue-300 transition-all duration-300"
+      />
+
+      <div className="absolute inset-y-0 left-0 pl-2 flex items-center">
+        <button
+          onClick={handleTogglePassword}
+          className="p-2 focus:outline-none focus:ring"
+        >
+          {showPassword ? (
+            <img
+              src={hide}
+              alt="Hide password"
+              className="h-6 w-6 text-gray-500"
+            />
+          ) : (
+            <img
+              src={hide}
+              alt="Show password"
+              className="h-6 w-6 text-gray-500"
+            />
+          )}
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default PasswordInput;
