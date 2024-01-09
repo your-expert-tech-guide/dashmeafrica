cb642a28d762866c6d0108387eaf7e1554635229
mutiat <mutiat900@gmail.com>
2024-01-09 02:08:47 +0100

    created the footer section

--- src/HomepageOne.jsx
+++ src/HomepageOne.jsx
@@ -103,6 +103,93 @@ return(
   
   </div>
 </section>
+
+{/*FOOTER */}
+<div className="">
+            <p className='text-center mt-8 font-medium text-md'>WANT TO PUT A SMILE ON SOMEONE’S FACE?</p>
+        <div className="flex justify-center mt-8">
+            <button
+        
+        className="inline-block font-lg mr-2 text-white rounded-lg border text-center border-solid bg-[#568203]  w-44 text-xs font-medium leading-normal text-black transition duration-150 ease-in-out hover:border-neutral-100 hover:bg-black-200  focus:border-#568203 focus:text-neutral-100 focus:outline-none focus:ring-0 active:border-black-200 active:text-dark:hover:bg-neutral-100 dark"
+      >
+        <Link to="/Signup2">Sell</Link>
+      </button>
+
+      <button
+        
+            className="inline-block font-lg text-[#568203] rounded-lg border border-solid border-[#568203] w-44 text-xs p-1 font-sm leading-normal text-[#568203] transition duration-150 ease-in-out hover:border-neutral-100 hover:bg-black-200  focus:border-#568203 focus:text-neutral-100 focus:outline-none focus:ring-0 active:border-black-200 active:text-dark:hover:bg-neutral-100 dark"
+          >
+            <Link to="/Signup2">Donate</Link>
+          </button>
+      </div>
+      <h2 className='text-center font-medium text-xs mt-8'><span className='text-[#568302]'>SIGN UP</span>   TO RECEIVE WEEKEND DISCOUNT OFFERS</h2>
+       
+      
+       
+       <footer className="px-4 divide-y dark:bg-gray-800 dark:text-gray-100">
+	<div className="container flex flex-col justify-between py-10 mx-auto space-y-8 lg:flex-row lg:space-y-0">
+		<div className="">
+        <div className="cart flex">
+            
+            {/* Image */}
+          <img
+            src={Dashcart}
+            alt="cart image"
+            className="w-44 mt-8 ml-12"
+          />
+          
+          </div>
+          <div className="socials flex gap-6 ml-16 mt-6">{/* Image */}
+          <img
+            src={Facebook}
+            alt="facebook icon"
+            className="w-6  "
+          />{/* Image */}
+          <img
+            src={Twitter}
+            alt="Twitter icon"
+            className="w-6"
+          />{/* Image */}
+          <img
+            src={Tik}
+            alt="Tiktok icon"
+            className="w-6 "
+          /></div>
+		</div>
+		<div className="  lg:w-2/3  flex gap-4">
+			<div className="space-y-3  mt-16 w-33">
+				<h3 className=" dark:text-gray-50 font-semibold">Our Mission</h3>
+				<p className=" space-y-1 text-xs  ">
+                To become the leading online <br /> marketplace where we help you  <br /> donate and/or sell
+                 your used <br /> items at giveaway prices, <br /> thereby reducing CO2 emission <br /> through our platform.
+				</p>
+			</div>
+			<div className="space-y-3 ml-10 mt-16 w-33">
+				<h3 className=" dark:text-gray-50 font-semibold">Nigeria</h3>
+				<div className="space-y-1 text-xs ">
+               <p className='mb-8'>No. 4B Wonodi Crescent <br /> by Stadium Road, Port Harcourt.</p>  
+                <span className='text-[#71033F] text-xs'>+234 703 675 6184 <br />help@dashme.africa</span>
+
+
+
+				</div>
+			</div>
+			<div className="space-y-3 ml-8 mt-16 w-33">
+				<h3 className="uppercase dark:text-gray-50 font-semibold">UK</h3>
+				<div className="space-y-1 text-xs ">
+				<p className='mb-8'>Andrexa Global Logistics, <br /> 8 Bromborough Avenue, <br />Wirral, Liverpool CH62 7AW.
+
+</p>	
+<span className='text-[#71033F] text-xs'>+447 799 178 814 <br />
+help@dashme.africa</span>
+				</div>
+			</div>
+			
+		</div>
+	</div>
+	
+</footer>
+        </div>
         </div>
 )
 
