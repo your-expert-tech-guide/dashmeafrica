42ec5c75a5750034161f808fe35a1e764b69f4bf
Joshua Okwor <chinazasundaygoodness@gmail.com>
2024-07-28 10:19:25 +0100

    completed auth pages

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,11 @@ export default {
   theme: {
     extend: {
       backgroundImage: {
-        "hero-pattern": "url('./src/assets/images/hero/hero-bg.png')",
-        "hero-pattern-m": "url('./src/assets/images/hero/hero-bg-m.png')",
-        "hero-pattern-md": "url('./src/assets/images/hero/hero-bg-md.png')",
-        "hero-pattern-5": "url('./src/assets/images/hero/hero-bg-5.png')",
-        "hero-pattern-9": "url('./src/assets/images/hero/hero-bg-9.png')",
+        "hero-pattern": "url('/src/assets/images/hero/hero-bg.png')",
+        "hero-pattern-m": "url('/src/assets/images/hero/hero-bg-m.png')",
+        "hero-pattern-md": "url('/src/assets/images/hero/hero-bg-md.png')",
+        "hero-pattern-5": "url('/src/assets/images/hero/hero-bg-5.png')",
+        "hero-pattern-9": "url('/src/assets/images/hero/hero-bg-9.png')",
       },
       scrollbar: {
         "&::-webkit-scrollbar": {
